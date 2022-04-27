34f9cb0f7634a8087c5dc62b36b490b4bde72c07
Diego Valenzuela <78625273+ValenzuelaDiego@users.noreply.github.com>
2022-04-26 23:30:30 -0400

    Add files via upload

---
+++ DailyEmailDigest/dd_gui.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+class DailyDigestGUI:
+    def __init__ (self, root):
+        pass #build the GUI
+
+if __name__ == '__main__':
+    root=Tk()  
+    app=DailyDigestGUI(root)
+    root.mainloop()  
